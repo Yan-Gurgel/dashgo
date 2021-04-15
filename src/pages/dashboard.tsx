d7da59dddfefb4e0f460e21f1c757628a7099dad
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-15 10:28:44 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,25 @@
-import { Header } from "../components/Form/Header";
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboad() {
     return (
-        <Header />
+
+        <Flex direction="column" h="100vh">
+
+            <Header />
+
+            <Flex
+                w="100%"
+                my="6"
+                mx="auto"
+                maxWidth={1480}
+                px="6"
+            >
+                <Sidebar />
+
+            </Flex>
+        </Flex>
+
     );
 }
