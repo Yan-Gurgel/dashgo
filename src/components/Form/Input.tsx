1ace7109715e212fec4228d2ff60c9dff5e05771
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-16 14:26:57 -0300

    Form de autenticacao

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,29 +1,34 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
 
-interface InputProps extends ChakraInputProps{
+interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
 }
 
 
-export function Input({name, label, ...rest}: InputProps) {
-    return (
-        <FormControl>
-            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput
-                name={name}
-                id={name}
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+    = ({ name, label, ...rest }, ref) => {
+        return (
+            <FormControl>
+                {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+                <ChakraInput
+                    name={name}
+                    id={name}
+                    focusBorderColor="pink.500"
+                    bgColor="gray.900"
+                    variant="filled"
 
-                _hover={{
-                    bgColor: 'gray.900'
-                }}
-                size="lg"
-                {...rest}
-            />
-        </FormControl>
+                    _hover={{
+                        bgColor: 'gray.900'
+                    }}
+                    size="lg"
+                    ref={ref}
+                    {...rest}
+                />
+            </FormControl>
 
-    );
-}
+        );
+    }
+
+export const Input = forwardRef(InputBase);
