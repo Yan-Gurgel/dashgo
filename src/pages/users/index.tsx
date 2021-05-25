5c2c1edaecc8cd7bcf44be8b82afee7c81c41b06
Yan-Gurgel <yan.gurgel@gmail.com>
2021-05-25 11:00:14 -0300

    Cadastro de user com mutations e SSR no React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,21 +5,24 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useUsers } from '../../services/hooks/useUsers';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
 import { useState } from 'react';
 import { queryClient } from '../../services/queryClilent';
 import { api } from '../../services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
+export default function UserList({users}) {
     const [page, setPage] = useState(1);
-    const {data, isLoading, isFetching ,error} = useUsers(page)
+    const {data, isLoading, isFetching ,error} = useUsers(page, {
+        initialData: users,
+    })
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
 
-    async function handlePrefetchUser(userId: number){
+    async function handlePrefetchUser(userId: String){
         await queryClient.prefetchQuery(['user', userId], async () => {
             const response = await api.get(`users/${userId}`)
 
@@ -113,3 +116,14 @@ export default function UserList() {
         </Box>
     );
 }
+/*
+export const getServerSideProps: GetServerSideProps = async () => {
+    const { users, totalCount} = await getUsers(1) 
+
+    return{
+        props: {
+            users,
+        }
+    }
+}
+*/
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { number } from "yup/lib/locale";
 import { api } from "../api";
 
@@ -43,8 +43,9 @@ const totalCount = Number(headers['x-total-count'])
 }
 
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
-    staleTime: 1000 * 60 * 10 //10 minutos de dados 'fresh'
-})
+    staleTime: 1000 * 60 * 10, //10 minutos de dados 'fresh'
+    ...options, 
+  })
 }
